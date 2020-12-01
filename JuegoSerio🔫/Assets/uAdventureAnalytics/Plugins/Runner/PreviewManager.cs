145809c61402cb019ca6a584f5ed0c78a7baa4b7
georgimednikov <gmedniko@ucm.es>
2020-12-01 16:24:35 +0100

    .gitignore is now working

--- JuegoSerio🔫/Assets/uAdventureAnalytics/Plugins/Runner/PreviewManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using uAdventure.Runner;
-
-public class PreviewManager : Singleton<PreviewManager>
-{
-	public bool InPreviewMode { get; set; }
-}
