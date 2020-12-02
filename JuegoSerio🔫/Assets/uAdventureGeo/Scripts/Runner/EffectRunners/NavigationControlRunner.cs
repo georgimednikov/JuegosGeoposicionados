ad1a418220db391da7d8a84dfcec51b03e0432d4
mmur07 <mmur@ucm.es>
2020-12-02 17:22:47 +0100

    👨‍🦼👨‍🦼👨‍🦼👨‍🦼
    aaa

---
+++ JuegoSerio🔫/Assets/uAdventureGeo/Scripts/Runner/EffectRunners/NavigationControlRunner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using uAdventure.Runner;
+using System;
+using uAdventure.Core;
+
+namespace uAdventure.Geo
+{
+    [CustomEffectRunner(typeof(NavigationControlEffect))]
+    public class NavigationControlRunner : CustomEffectRunner
+    {
+        private NavigationControlEffect navEffect;
+        public IEffect Effect { get { return navEffect; } set { navEffect = value as NavigationControlEffect; } }
+
+        public NavigationControlRunner()
+        {
+            if (NavigationController.Instance == null)
+            {
+                var navigationPrefab = Resources.Load<GameObject>("navigation");
+                if (navigationPrefab != null)
+                {
+                    GameObject.Instantiate(navigationPrefab);
+                }
+            }
+        }
+
+        public bool execute()
+        {
+            return false;
+        }
+    }
+}
